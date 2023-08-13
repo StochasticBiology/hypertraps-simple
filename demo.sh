390d26aa08f2d984915cf5d3dc163f33739262ad
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2023-08-13 19:18:13 +0200

    Add files via upload

---
+++ demo.sh
@@ -0,0 +1,7 @@
+date
+gcc -o3 hypertraps.c -lm -o hypertraps.ce
+./hypertraps.ce fake-cross-samples-1.txt 1 1 5 0
+date
+gcc -o3 posteriors.c -lm -o posteriors.ce
+./posteriors.ce 1 fake-cross-samples-1.txt-posterior-0-1-1-5.txt
+date
